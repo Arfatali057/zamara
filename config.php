2fb539402b2080ada142a49a4fdde03924a8d468
Arfatali <86524897+Arfatali057@users.noreply.github.com>
2024-09-04 20:20:48 +0500

    Add files via upload

---
+++ config.php
@@ -0,0 +1,10 @@
+<?php
+
+if (!defined('BOT_OWNER_NAME'))                      define('BOT_OWNER_NAME', 'arafat ali');
+if (!defined('BOT_OWNER_USERNAME'))                      define('BOT_OWNER_USERNAME', 'arfat9696');
+if (!defined('BOT_OWNER_ID'))                      define('BOT_OWNER_ID', '1953498950');
+
+if (!defined('BOT_NAME'))               define('BOT_NAME', 'fuck');
+if (!defined('BOT_USERNAME'))               define('BOT_USERNAME', 'zamara96_bot');
+if (!defined('BOT_TOKEN'))                       define('BOT_TOKEN', '7489596803:AAHkEaJO9dl4qm4sIWH1SR16M3F7fID-iuA');
+
